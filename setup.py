c8b9f0b58c7516facd578349f15b1806ab44f537
Benny <Benny@gmx.it>
2014-08-10 17:30:31 +0200

    Bump to version 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Yahoo-ticker-downloader',
-    version = '0.2.8',
+    version = '0.3.0',
     author = 'Benny',
     author_email = 'Benny@GMX.it',
     url='https://github.com/Benny-/Yahoo-ticker-symbol-downloader',
